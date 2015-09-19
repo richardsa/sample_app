44587625430a96c875bb1d3564546540d29f6fe4
richie <rsaldiva@gmail.com>
2015-09-18 21:26:03 -0400

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,6 +63,10 @@ class User < ActiveRecord::Base
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+   # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   private
 
